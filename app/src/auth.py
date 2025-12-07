8d5422cbbbdb2128bea077904163ddaa079f8641
Sheldon Ledbetter <sdledbetter@arizona.edu>
2025-12-06 23:24:56 -0500

    Adjusted flash

--- app/src/auth.py
+++ app/src/auth.py
@@ -46,7 +46,7 @@ def login():
                     return redirect(url_for('views.home'))
                 
                 else:
-                    flash('Your account is currently set to disabled.', category='warning')
+                    flash('Your account is currently set to disabled.', category='danger')
                     return render_template("auth/login.html", user=current_user)
             else:
                 flash('Incorrect username/password.', category='danger')
